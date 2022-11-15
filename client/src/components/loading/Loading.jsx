f83a102fd5df0a0059621ebd199129b19874c3ef
Jairo Casalins <jairo.casalins@i2btech.com>
2022-11-15 03:44:10 -0500

    fix

--- client/src/components/loading/Loading.jsx
+++ client/src/components/loading/Loading.jsx
@@ -3,7 +3,7 @@ import './loading.scss'
 function Loading() {
   return (
     <div className="loading">
-      <div class="lds-ellipsis">
+      <div className="lds-ellipsis">
         <div></div>
         <div></div>
         <div></div>
--- client/src/environments/environment.jsx
+++ client/src/environments/environment.jsx
@@ -1,6 +1,7 @@
-export default env = {
+const env = {
     BASE_URL: '/api',
     HOST: 'localhost',
     PORT: 3000,
     PROTOCOL: 'http'
 }
+export default env;
