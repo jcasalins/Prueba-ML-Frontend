5553ba504b57c58db7b02b9797dabb8c4ec606b6
Jairo Casalins <jairo.casalins@i2btech.com>
2022-11-15 01:12:24 -0500

    componente Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/header/Header'
-import Home from './pages/home/Home'
-import Items from './pages/items/Items'
-import Item from './pages/item/Item'
-
+import AppRoutes from './routes/Index'
 function App() {
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/items" element={<Items />} />
-          <Route path="/items/:id" element={<Item />} />
-          <Route path="*" element={<main>404 Not Found</main>} />
-        </Routes>
-      </Router>
+      <AppRoutes />
     </>
   )
 }
