502c9112dfe6e1e2f266aa041b5ec1dd2a631043
Jairo Casalins <jairo.casalins@i2btech.com>
2022-11-15 03:58:13 -0500

    .env

--- backend/src/utils/env.utils.js
+++ backend/src/utils/env.utils.js
@@ -1,7 +1,7 @@
 import * as dotenv from 'dotenv'
 import path from 'path'
 dotenv.config({
-  path: path.resolve(process.cwd() + '/../.env')
+  path: path.resolve(process.cwd() + '/.env')
 })
 const ENV = {
   ML_URL_HTTPS: process.env.ML_URL_HTTPS,
